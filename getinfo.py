0e7dc499210888d66c2bc660dd9c0201d37a2575
Wintoch <kub123-2001@wp.pl>
2026-09-02 22:41:51 +0200

    Reading mode not working properly yet

---
+++ getinfo.py
@@ -0,0 +1,21 @@
+import serial
+import time
+
+ser = serial.Serial('COM3', 115200, timeout=1)
+
+#waking up the module
+ser.write(b'\x55\x55\x00\x00\x00\x00\x00\x00\x00\x00')
+
+turnAntenaOn = b'\x00\x00\xFF\x03\xFD\xD4\x14\x01\x17\x00'
+ser.write(turnAntenaOn)
+
+clear_buffer = ser.read(15)
+
+#InListPassiveTarget instruction
+listenerMode = b'\x00\x00\xFF\x04\xFC\xD4\x4A\x01\x00\xE1\x00'
+ser.write(listenerMode)
+
+while(True):
+    response = ser.read(100)
+    if(response):
+        print(response.hex(' ').upper())
--- test_connection.py
+++ test_connection.py
@@ -2,17 +2,15 @@ import serial
 import time
 
 ser = serial.Serial('COM3', 115200, timeout=1)
-time.sleep(1)
+
+##### SHORT COMMAND ASKING FOR OS VERSION
 
 ser.write(b'\x55\x55\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
-time.sleep(0.1)
 
 frimware_cmd = b'\x00\x00\xFF\x02\xFE\xD4\x02\x2A\x00'
 print("Sending the frame")
 
 ser.write(frimware_cmd)
-
-time.sleep(0.1)
 
 response = ser.read(100)
 hex_response = response.hex(' ').upper()
