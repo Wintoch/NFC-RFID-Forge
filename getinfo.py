12df61a804c75bd3eb50e9735adb67506571c47b
Wintoch <kub123-2001@wp.pl>
2026-09-04 20:59:57 +0200

    get basic info from card - ready

--- getinfo.py
+++ getinfo.py
@@ -11,11 +11,23 @@ ser.write(turnAntenaOn)
 
 clear_buffer = ser.read(15)
 
-#InListPassiveTarget instruction
-listenerMode = b'\x00\x00\xFF\x04\xFC\xD4\x4A\x01\x00\xE1\x00'
-ser.write(listenerMode)
+def getBasicInfo(onlySAK):
+    #InListPassiveTarget instruction
+    listenerMode = b'\x00\x00\xFF\x04\xFC\xD4\x4A\x01\x00\xE1\x00'
 
-while(True):
-    response = ser.read(100)
-    if(response):
-        print(response.hex(' ').upper())
+    ser.write(listenerMode)
+    #ingoring the first 6 bytes
+    ser.read(6)
+
+    #getting the type of card and its basic informations
+    while(True):
+        response = ser.read(100)
+        if(response):
+            if(onlySAK):
+                return response[11], response[13 : 13 + response[12]]
+            print('Basic card info: ', response.hex(' ').upper())
+            ser.write(listenerMode)
+            ser.read(6)
+
+def getUsedTechnologyInfo():
+    sak = getBasicInfo(True)
